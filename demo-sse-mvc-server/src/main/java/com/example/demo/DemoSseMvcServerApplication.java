6a18fdfc4641aa0b7fefe54b9187c69c47b3993a
justinliu <nhliupm@aliyun.com>
2023-06-04 18:09:12 +0800

    expose management endpoints

--- demo-sse-mvc-server/src/main/java/com/example/demo/DemoSseMvcServerApplication.java
+++ demo-sse-mvc-server/src/main/java/com/example/demo/DemoSseMvcServerApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoSseMvcServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSseMvcServerApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "demo-sse-mvc-server");
     }
 
 }
