f6ee68d7288e7f2c841fdcf547e8aa9b15347451
justinliu <nhliupm@aliyun.com>
2023-06-06 10:05:20 +0800

    add house keeping

---
+++ demo-sse-mvc-server/src/main/java/com/example/demo/service/HouseKeepingScheduler.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.SseEventRepository;
+import com.example.demo.entity.SseEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class HouseKeepingScheduler {
+    private final SseEventRepository sseEventRepository;
+
+    @Value("${sse-mvc-server.housekeeping.history-in-hours}")
+    private long historyInHours;
+
+    @Scheduled(cron = "${sse-mvc-server.housekeeping.scheduler}")
+    public void deleteSseEvents() {
+        LocalDateTime before = LocalDateTime.now().minusHours(historyInHours);
+        List<SseEvent> sseEvents = sseEventRepository.findByCreatedDttmBefore(before);
+        if (!CollectionUtils.isEmpty(sseEvents)) {
+            sseEventRepository.deleteAll(sseEvents);
+            log.info("Deleted historical sseEvents: size={}", sseEvents.size());
+        }
+    }
+}
